651606521c9068f333806f681dbef115aaf44dc1
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-20 18:21:17 +0100

    04 building images: Removing images

--- 04-building-images/cmd.bash
+++ 04-building-images/cmd.bash
@@ -576,3 +576,129 @@ docker build -t react-app-mosh .
 #     |
 #     | changing instructions
 #     v changing instructions
+
+
+# 12 - Removing Images 02h17:20
+# ------------------------------
+
+# run 
+docker images
+
+# see all dangling images
+docker images
+REPOSITORY                                    TAG       IMAGE ID       CREATED        SIZE
+react-app-mosh                                latest    f8b1c5a1f044   4 hours ago    685MB
+<none>                                        <none>    ef30cd37f3a1   4 hours ago    685MB # <-- this !
+<none>                                        <none>    2952693e5467   6 hours ago    444MB # <-- this !
+<none>                                        <none>    1bae1a887fc3   6 hours ago    444MB # <-- this !
+<none>                                        <none>    1ea19203af7a   22 hours ago   444MB # <-- this !
+laith-harb-node-app-01-building-image-final   latest    96d955585f1c   8 days ago     168MB
+hello-docker-mosh                             latest    f2acf5276833   12 days ago    159MB
+ubuntu                                        latest    59ab366372d5   5 weeks ago    78.1MB
+postgres                                      17        d57ed788c154   7 weeks ago    434MB
+alpine                                        latest    63b790fccc90   2 months ago   7.8MB
+
+
+# remove those dangling images
+docker image prune
+
+
+# remove all stopped containers
+docker container prune 
+
+# run the following to get all infos about image cmd option
+$ docker image
+
+Usage:  docker image COMMAND
+
+Manage images
+
+Commands:
+  build       Build an image from a Dockerfile
+  history     Show the history of an image
+  import      Import the contents from a tarball to create a filesystem image
+  inspect     Display detailed information on one or more images
+  load        Load an image from a tar archive or STDIN
+  ls          List images
+  prune       Remove unused images
+  pull        Download an image from a registry
+  push        Upload an image to a registry
+  rm          Remove one or more images
+  save        Save one or more images to a tar archive (streamed to STDOUT by default)
+  tag         Create a tag TARGET_IMAGE that refers to SOURCE_IMAGE
+
+Run 'docker image COMMAND --help' for more information on a command.
+
+
+# run the following cmd:
+
+docker image rm --help
+
+Usage:  docker image rm [OPTIONS] IMAGE [IMAGE...]
+
+Remove one or more images
+
+Aliases:
+  docker image rm, docker image remove, docker rmi
+
+Options:
+  -f, --force      Force removal of the image
+      --no-prune   Do not delete untagged parents
+
+
+$ docker images
+REPOSITORY                                    TAG       IMAGE ID       CREATED        SIZE
+react-app-mosh                                latest    f8b1c5a1f044   4 hours ago    685MB
+laith-harb-node-app-01-building-image-final   latest    96d955585f1c   8 days ago     168MB
+hello-docker-mosh                             latest    f2acf5276833   12 days ago    159MB
+ubuntu                                        latest    59ab366372d5   5 weeks ago    78.1MB
+postgres                                      17        d57ed788c154   7 weeks ago    434MB
+alpine                                        latest    63b790fccc90   2 months ago   7.8MB
+
+
+docker image rm hello-docker
+
+#check: 
+docker images
+
+# confirm the targetted image is not listed
+
+
+# also run the cmd:
+
+docker container
+
+Usage:  docker container COMMAND
+
+Manage containers
+
+Commands:
+  attach      Attach local standard input, output, and error streams to a running container
+  commit      Create a new image from a container's changes
+  cp          Copy files/folders between a container and the local filesystem
+  create      Create a new container
+  diff        Inspect changes to files or directories on a container's filesystem
+  exec        Execute a command in a running container
+# export      Export a container's filesystem as a tar archive
+  inspect     Display detailed information on one or more containers
+  kill        Kill one or more running containers
+  logs        Fetch the logs of a container
+  ls          List containers
+  pause       Pause all processes within one or more containers
+  port        List port mappings or a specific mapping for the container
+  prune       Remove all stopped containers
+  rename      Rename a container
+  restart     Restart one or more containers
+  rm          Remove one or more containers
+  run         Create and run a new container from an image
+  start       Start one or more stopped containers
+  stats       Display a live stream of container(s) resource usage statistics
+  stop        Stop one or more running containers
+  top         Display the running processes of a container
+  unpause     Unpause all processes within one or more containers
+  update      Update configuration of one or more containers
+  wait        Block until one or more containers stop, then print their exit codes
+
+Run 'docker container COMMAND --help' for more information on a command.
+
+# change color
