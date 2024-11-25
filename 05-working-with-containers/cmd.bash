05ce5ebbe6e9ab2b66985baab596b4c71de1cd1f
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-25 15:36:33 +0100

    05 working with containers:Publishing port,  Execute cmd on running container, Start& Stopping container

--- 05-working-with-containers/cmd.bash
+++ 05-working-with-containers/cmd.bash
@@ -1,10 +1,10 @@
-# 1 - Starting Containers
-# -------------------------
+# 1 - Starting Containers 02h34:04
+# ---------------------------------
 
 # check images
 docker  images
 
-#see running container(s)
+#see running container(s) 
 docker ps
 
 # run a new container, 
@@ -21,8 +21,8 @@ docker ps
 docker run -d --name blue-sky react-app-mosh:v3 
 
 
-# 2 - Viewing the logs
-# ---------------------
+# 2 - Viewing the logs 02h347:07
+# ------------------------------
 
 # print the log of a container that is running in detached mode
 
@@ -57,14 +57,64 @@ docker logs -t 03d0
 docker logs -tf 03d0
 
 
-# 3 - Publishing Ports 
-# -------------------------
+# 3 - Publishing Ports 02h39:15
+# -------------------------------
 
-# 4 - Executing Cmds in Running Containers 
-# -----------------------------------------
+# start a new container in detached mode and publish the port at the same time
+docker run -d -p 82:3000 --name cont-1 react-app-mosh:v3
 
-# 5 - Stopping & Starting Containers
-# -----------------------------------
+#check the running containers
+docker ps
+
+CONTAINER ID   IMAGE               COMMAND                  CREATED              STATUS              PORTS                  NAMES
+34d33ea13c54   react-app-mosh:v3   "docker-entrypoint.s…"   About a minute ago   Up About a minute   0.0.0.0:82->3000/tcp   cont-1
+03d04160cafc   react-app-mosh:v3   "docker-entrypoint.s…"   23 minutes ago       Up 23 minutes       3000/tcp               blue-sky
+fbfcf539dc72   react-app-mosh:v3   "docker-entrypoint.s…"   28 minutes ago       Up 28 minutes       3000/tcp               dazzling_euclid
+
+# 4 - Executing Cmds in Running Containers 02h41:21
+# --------------------------------------------------
+
+# show the file system for a running container
+
+# print the working directory
+docker exec cont-1 pwd
+/app
+
+# print the content of the woring directory
+docker exec cont-1 ls
+Dockerfile
+README.md
+node_modules
+package-lock.json
+package.json
+public
+src
+
+# open a shell session in interactive mode in a running container 
+docker exec -it cont-1 sh
+
+# exit
+exit
+
+# 5 - Stopping & Starting Containers 02h43:19
+# --------------------------------------------
+
+# stop a running container
+docker stop cont-1
+
+#check 
+docker ps
+
+CONTAINER ID   IMAGE               COMMAND                  CREATED          STATUS         PORTS      NAMES
+03d04160cafc   react-app-mosh:v3   "docker-entrypoint.s…"   40 minutes ago   Up 9 minutes   3000/tcp   blue-sky
+fbfcf539dc72   react-app-mosh:v3   "docker-entrypoint.s…"   45 minutes ago   Up 9 minutes   3000/tcp   dazzling_euclid
+
+# refresh the browser page at port 82, and CONFIRM the application is NOT RUNNING anymore
+
+# start again the stopped container cont-1
+docker start cont-1
+
+# refresh the browser page at port 82, and CONFIRM the application is RUNNING effectively
 
 # 6 - Removing Containers
 # -------------------------
