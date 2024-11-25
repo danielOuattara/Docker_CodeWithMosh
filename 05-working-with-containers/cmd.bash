c113feab17376408194ac9764f7f4e18f015c21a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-25 14:40:58 +0100

    05 working with containers: starting

---
+++ 05-working-with-containers/cmd.bash
@@ -0,0 +1,29 @@
+# 1 - Starting Containers
+# -------------------------
+
+# 2 - Viewing the logs
+# -------------------------
+
+# 3 - Publishin Ports 
+# -------------------------
+
+# 4 - Executing Cmds in Running Containers 
+# -----------------------------------------
+
+# 5 - Stopping & Starting Containers
+# -----------------------------------
+
+# 6 - Removing Containers
+# -------------------------
+
+# 7 - Container File System
+# -------------------------
+
+# 8 - Persisting Data using Volumes
+# ---------------------------------
+
+# 9 - Copying Files between the host and containers 
+# -------------------------------------------------
+
+# 10 - Sharing the source  code with a Container
+# -----------------------------------------------
